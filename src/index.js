891e6ccce2d0a2f82a58f2730aafa63c442069ec
Hamit Mızrak <hamitmizrak@gmail.com>
2025-11-24 15:22:11 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
+// singele comment
+/* 
+JS multiple comment
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+// external css
 import './index.css';
+
+// external
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Counter from './app/Counter';
+import { BrowserRouter } from 'react-router-dom';
+import ResuabilityToast from './reusability/ResuabilityToast';
 
+// Root
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* <ResuabilityToast></ResuabilityToast> */}
+      <ResuabilityToast />
+      {/* <App /> */}
+      {/* <Counter /> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
